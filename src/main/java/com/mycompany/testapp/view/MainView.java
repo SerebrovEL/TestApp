f6d1b4967ee25e10e9db28778e9891568f38976a
Серебров Евгений Леонидович <serebrov_e_l@mail.ru>
2023-03-16 19:50:58 +0500

    Рефакторинг кода.

--- src/main/java/com/mycompany/testapp/view/MainView.java
+++ src/main/java/com/mycompany/testapp/view/MainView.java
@@ -1,9 +1,7 @@
 package com.mycompany.testapp.view;
 
 import com.mycompany.testapp.model.Mkb10;
-import com.mycompany.testapp.model.Mkb10TreeView;
 import com.mycompany.testapp.services.DataService;
-import com.vaadin.componentfactory.explorer.ExplorerTreeGrid;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.MultiSortPriority;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
@@ -11,30 +9,15 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.data.provider.hierarchy.TreeData;
-import com.vaadin.flow.data.provider.hierarchy.TreeDataProvider;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Route
 public class MainView extends VerticalLayout {
-
-	private Mkb10TreeView getTreeViewElem(Mkb10 mkb10, Collection<Mkb10> mkb10s) {
-		if (mkb10 == null) {
-			return null;
-		} else {
-			if (mkb10.getCodeParent() == null) {
-				return new Mkb10TreeView(mkb10.getCode(), mkb10.getName(), null);
-			} else {
-				Mkb10 t = mkb10s.stream()
-					.filter(el -> el.getCode().equalsIgnoreCase(mkb10.getCodeParent()))
-					.findAny().orElse(null);
-				return new Mkb10TreeView(mkb10.getCode(), mkb10.getName(), getTreeViewElem(t, mkb10s));
-			}
-		}
-	}
 
 	public MainView() {
 		Grid<Mkb10> grid = new Grid<Mkb10>(Mkb10.class, false);
@@ -63,22 +46,35 @@ public class MainView extends VerticalLayout {
 			boolean matchesCodeParent = matchesTerm(mkb10.getCodeParent(), searchTerm);
 			return matchesCode || matchesName || matchesCodeParent;
 		});
-
-		Collection<Mkb10TreeView> treeViews = new ArrayList<>();
-		for (var elem : mkb10s) {
-			treeViews.add(getTreeViewElem(elem, mkb10s));
-		}
-		TreeData<Mkb10TreeView> treeData = new TreeData<>();
-		treeData.addRootItems(treeViews);
-		TreeDataProvider<Mkb10TreeView> inMemoryDataProvider = new TreeDataProvider<>(treeData);
-		ExplorerTreeGrid<Mkb10TreeView> grid1 = new ExplorerTreeGrid<>();
-		grid1.addHierarchyColumn(Mkb10TreeView::getCode).setHeader("Код");
-		grid1.setItems(inMemoryDataProvider);
 		
-		VerticalLayout layout = new VerticalLayout(searchField, grid1, grid);
-		layout.setPadding(false);
+		TreeData<String> data = new TreeData<>();
+		Collection<String> root = mkb10s.stream()
+			.filter(elem -> elem.getCodeParent().isEmpty())
+			.map(elem -> elem.getCode())
+			.distinct()
+			.collect(Collectors.toList());
+		for (var elem : root) {
+			if (!data.contains(elem)) {
+				data.addItems(null, elem);
+			}
+		}
+		data.getRootItems().forEach(elem -> {
+			Collection<String> temp = mkb10s.stream()
+				.filter(el -> el.getCodeParent().contains(elem))
+				.map(el -> el.getCode())
+				.distinct()
+				.collect(Collectors.toList());
+			data.addItems(elem, temp);
+		});
+		TreeGrid<String> treeGrid = new TreeGrid<>();
+		treeGrid.setTreeData(data);
+		
+		VerticalLayout hLayout = new VerticalLayout(treeGrid, grid);
+		hLayout.setPadding(true);
+		VerticalLayout vLayout = new VerticalLayout(searchField, hLayout);
+		vLayout.setPadding(false);
 
-		add(layout);
+		add(vLayout);
 	}
 
 	private boolean matchesTerm(String value, String searchTerm) {
