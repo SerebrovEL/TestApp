02620a2d6d8d7db48bd1727618307a35d65c085a
Серебров Евгений Леонидович <serebrov_e_l@mail.ru>
2023-03-19 02:35:40 +0500

    Багфиксинг кода. Рефакторинг UI.

--- src/main/java/com/mycompany/testapp/model/Mkb10TreeData.java
+++ src/main/java/com/mycompany/testapp/model/Mkb10TreeData.java
@@ -10,38 +10,106 @@ public class Mkb10TreeData {
 	protected Collection<Mkb10Data> dataList;
 
 	public Mkb10TreeData() {
-		this.dataList = null;
+		this.dataList = new ArrayList<>();
 	}
 
-	public Collection<Mkb10Data> createTreeData(Collection<Mkb10> mkb10s) {
-		this.dataList = new ArrayList<>();
-		Collection<Mkb10> roots = mkb10s.stream()
-			.filter(elem -> elem.getCodeParent() == null
-			|| elem.getCodeParent().isEmpty())
-			.collect(Collectors.toList());
-		roots.forEach(elem -> this.dataList.add(new Mkb10Data(elem.getCode(), elem.getName(), "", null)));
-		Collection<Mkb10> childs = mkb10s.stream()
-			.filter(elem -> elem.getCodeParent() != null
-			&& !elem.getCodeParent().isEmpty())
-			.collect(Collectors.toList());
-		for (var elem : childs) {
-			Mkb10Data root = this.dataList.stream()
-				.filter(el -> el.getCode().equalsIgnoreCase(elem.getCodeParent()))
-				.findFirst().orElse(null);
-			this.dataList.add(new Mkb10Data(elem.getCode(), elem.getName(), "", root));
-		}
-		return this.dataList;
+	public void createTreeData(Collection<Mkb10> mkb10s) {
+		addRoot(mkb10s);
+		addChild(mkb10s);
 	}
 
 	public Collection<Mkb10Data> getRootTreeData() {
 		return this.dataList.stream()
 			.filter(treeDataList -> treeDataList.getParent() == null)
+			.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
 			.collect(Collectors.toList());
 	}
 
 	public Collection<Mkb10Data> getChildTreeData(Mkb10Data parent) {
-		return this.dataList.stream().filter(
-			treeDataList -> Objects.equals(treeDataList.getParent(), parent))
+		return this.dataList.stream()
+			.filter(treeDataList -> Objects.equals(treeDataList.getParent(), parent))
+			.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
 			.collect(Collectors.toList());
 	}
+
+	protected void addRoot(Collection<Mkb10> mkb10s) {
+		Collection<Mkb10> roots = mkb10s.stream()
+			.filter(elem -> elem.getCodeParent() == null
+			|| elem.getCodeParent().trim().isEmpty()
+			|| elem.getCodeParent().trim().isBlank()
+			|| elem.getCodeParent().trim().contains("NULL"))
+			.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
+			.distinct()
+			.collect(Collectors.toList());
+		roots.forEach(elem -> this.dataList.add(new Mkb10Data(elem.getCode(), elem.getName(), "", null)));
+		roots.forEach(root -> {
+			mkb10s.stream()
+				.filter(elem -> elem.getCodeParent() != null
+				&& !elem.getCodeParent().trim().isEmpty()
+				&& elem.getCodeParent().trim().equalsIgnoreCase(root.getCode().trim())
+				)
+				.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
+				.collect(Collectors.toList())
+				.forEach(
+					elem
+					-> {
+					this.dataList
+						.add(
+							new Mkb10Data(
+								elem.getCode().trim(),
+								elem.getName().trim(),
+								"",
+								this.dataList
+									.stream()
+									.filter(el -> el.getCode()
+									.trim()
+									.equalsIgnoreCase(root.getCode().trim())
+									).findFirst()
+									.orElse(null)
+							)
+						);
+				}
+				);
+		});
+	}
+
+	protected void addChild(Collection<Mkb10> mkb10s) {
+		int i = 0;
+		while (true) {
+			final int level = ++i;
+			Collection<Mkb10Data> par = this.dataList.stream()
+				.filter(elem -> elem.getParent() != null && elem.getLevel() == level)
+				.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
+				.collect(Collectors.toList());
+			if (par.isEmpty()) {
+				break;
+			}
+			for (Mkb10Data root : par) {
+				Collection<Mkb10> childs = mkb10s.stream()
+					.filter(elem -> elem.getCodeParent() != null
+					&& !elem.getCodeParent().trim().isEmpty()
+					&& elem.getCodeParent().trim().equalsIgnoreCase(root.getCode().trim())
+					)
+					.sorted((o1, o2) -> o1.getCode().trim().compareTo(o2.getCode().trim()))
+					.collect(Collectors.toList());
+				for (Mkb10 elem : childs) {
+					this.dataList
+						.add(
+							new Mkb10Data(
+								elem.getCode().trim(),
+								elem.getName().trim(),
+								"",
+								this.dataList
+									.stream()
+									.filter(el -> el.getCode()
+									.trim()
+									.equalsIgnoreCase(root.getCode().trim()))
+									.findFirst().orElse(null)
+							)
+						);
+				}
+			}
+		}
+	}
+
 }
--- src/main/java/com/mycompany/testapp/view/MainView.java
+++ src/main/java/com/mycompany/testapp/view/MainView.java
@@ -5,12 +5,11 @@ import com.mycompany.testapp.model.Mkb10Data;
 import com.mycompany.testapp.model.Mkb10TreeData;
 import com.mycompany.testapp.services.DataService;
 import com.vaadin.componentfactory.explorer.ExplorerTreeGrid;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.MultiSortPriority;
-import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -18,70 +17,81 @@ import com.vaadin.flow.router.Route;
 import java.util.Collection;
 
 @Route
-public class MainView extends VerticalLayout {
+public class MainView extends HorizontalLayout {
 
 	public MainView() {
+		Collection<Mkb10> mkb10s = (new DataService()).getData();
+		Mkb10TreeData mkb10TreeData = new Mkb10TreeData();
+		mkb10TreeData.createTreeData(mkb10s);
+
 		Grid<Mkb10> grid = new Grid<Mkb10>(Mkb10.class, false);
 		grid.addColumn(Mkb10::getCode).setHeader("Код").setSortable(true);
 		grid.addColumn(Mkb10::getName).setHeader("Наименование").setSortable(true);
 		grid.addColumn(Mkb10::getCodeParent).setHeader("Код родителя").setSortable(true);
 		grid.setMultiSort(true, MultiSortPriority.APPEND);
-
-		Collection<Mkb10> mkb10s = (new DataService()).getData();
-		GridListDataView<Mkb10> dataView = grid.setItems(mkb10s);
+		grid.setItems(mkb10s);
 
 		TextField searchField = new TextField();
 		searchField.setWidth("50%");
 		searchField.setPlaceholder("Search");
 		searchField.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
 		searchField.setValueChangeMode(ValueChangeMode.EAGER);
-		searchField.addValueChangeListener(e -> dataView.refreshAll());
+		searchField.addValueChangeListener(e -> grid.getListDataView().refreshAll());
 
-		Mkb10TreeData mkb10TreeData = new Mkb10TreeData();
-		mkb10TreeData.createTreeData(mkb10s);
 		ExplorerTreeGrid<Mkb10Data> treeGrid = new ExplorerTreeGrid<>();
 		treeGrid.setItems(mkb10TreeData.getRootTreeData(), mkb10TreeData::getChildTreeData);
-		treeGrid.addHierarchyColumn(Mkb10Data::getCode).setHeader("Код");
+		treeGrid.addHierarchyColumn(Mkb10Data::getCode).setHeader("Меню (Код заболевания)");
 		treeGrid.setWidthFull();
 		treeGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+		treeGrid.setHeight("100%");
+		treeGrid.setWidth("20%");
 		treeGrid.addSelectionListener(listener -> {
-			dataView.removeFilters();
+			grid.getListDataView().removeFilters();
 			Mkb10Data sel = listener.getFirstSelectedItem().orElse(null);
 			if (sel != null) {
-				dataView.addFilter(mkb10 -> {
+				grid.getListDataView().addFilter(mkb10 -> {
 					String searchTerm = sel.getCode().trim();
 					if (searchTerm.isEmpty()) {
 						return true;
 					}
-					boolean matchesCode = matchesTerm(mkb10.getCode(), searchTerm);
-					return matchesCode;
+					return matchesTermCode(mkb10.getCode(), searchTerm);
 				});
 			} else {
-				dataView.removeFilters();
+				grid.getListDataView().removeFilters();
 			}
 		});
 		searchField.addKeyPressListener(listener -> {
 			treeGrid.deselectAll();
-			dataView.removeFilters();
-			dataView.addFilter(mkb10 -> {
+			grid.getListDataView().removeFilters();
+			grid.getListDataView().addFilter(mkb10 -> {
 				String searchTerm = searchField.getValue().trim();
 				if (searchTerm.isEmpty()) {
 					return true;
 				}
-				boolean matchesCode = matchesTerm(mkb10.getCode(), searchTerm);
+				boolean matchesCode = matchesTermCode(mkb10.getCode(), searchTerm);
 				boolean matchesName = matchesTerm(mkb10.getName(), searchTerm);
-				boolean matchesCodeParent = matchesTerm(mkb10.getCodeParent(), searchTerm);
+				boolean matchesCodeParent = matchesTermCode(mkb10.getCodeParent(), searchTerm);
 				return matchesCode || matchesName || matchesCodeParent;
 			});
 		});
 
-		VerticalLayout vLayout = new VerticalLayout(treeGrid, searchField, grid);
+		VerticalLayout vLayout = new VerticalLayout(searchField, grid);
 		vLayout.setPadding(true);
+		vLayout.setHeight("100%");
+			
+		this.add(treeGrid);
+		this.add(vLayout);
+		this.setVerticalComponentAlignment(Alignment.START, treeGrid);
+		this.setVerticalComponentAlignment(Alignment.START, vLayout);
+		this.setHeightFull();
 		
-		add(vLayout);
 	}
 
 	private boolean matchesTerm(String value, String searchTerm) {
 		return value.toLowerCase().contains(searchTerm.toLowerCase());
+	}
+
+	private boolean matchesTermCode(String value, String searchTerm) {
+		return value.trim().equalsIgnoreCase(searchTerm.trim());
 	}
 }
